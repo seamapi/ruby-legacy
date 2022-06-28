2dc3f25443e3581cf439e39d2eb938f1a8786e0a
Abimael Martell <abimex@gmail.com>
2022-06-28 00:04:13 -0700

    initial commit

---
+++ lib/seamapi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "seamapi/version"
+
+module Seamapi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/seamapi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Seamapi do
+  it "has a version number" do
+    expect(Seamapi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
