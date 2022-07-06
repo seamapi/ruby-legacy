056d64ff2b84788e0a66812b270aae92db44e074
seveibar <seveibar@gmail.com>
2022-07-05 23:05:56 -0700

    fix stubber

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -4,7 +4,7 @@ module Helpers
   def stub_seam_request(method, path, body, status = 200, headers = {})
     stub_request(
       method,
-      "https://connect.getseam.com/#{path}"
+      "https://connect.getseam.com#{path}"
     ).to_return(status: status, body: body.to_json,
                 headers: { "Content-Type" => "application/json" }.merge(headers))
   end
