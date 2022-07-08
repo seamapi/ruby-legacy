57ee95d72f38138c5faf3d88d5d0c8fc71528d65
Abimael Martell <abimex@gmail.com>
2022-07-08 08:50:23 -0700

    fix action atttempt wait_until_finished

--- lib/seam/resources/action_attempt.rb
+++ lib/seam/resources/action_attempt.rb
@@ -4,17 +4,28 @@ module Seam
   class ActionAttempt < BaseResource
     attr_accessor :action_attempt_id, :action_type, :status, :result
 
+    SLEEP_TIME = 0.2
+
     def wait_until_finished
       while @status == "pending"
-        res = Seam::Request.new(@client.api_key, @client.base_uri).perform(
-          :get,
-          "/action_attempts/get",
-          params: { action_attempt_id: @action_attempt_id }
-        )
-        update_from_response(res["action_attempt"])
-        sleep(0.2)
+        update!
+
+        sleep(SLEEP_TIME)
       end
+
       self
+    end
+
+    def update!
+      response = @client.request_seam(
+        :get,
+        "/action_attempts/get",
+        params: {
+          action_attempt_id: action_attempt_id
+        }
+      )
+
+      update_from_response(response["action_attempt"])
     end
   end
 end
