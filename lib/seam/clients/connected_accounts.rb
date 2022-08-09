89bc75df709b392852f1910b440effa1f3d615ec
Abimael Martell <abimex@gmail.com>
2022-08-09 13:08:42 -0700

    add connected account list and get by email

--- lib/seam/clients/connected_accounts.rb
+++ lib/seam/clients/connected_accounts.rb
@@ -3,13 +3,23 @@
 module Seam
   module Clients
     class ConnectedAccounts < BaseClient
-      def get(connected_account_id = nil)
+      def get(connected_account_id: nil, email: nil)
         request_seam_object(
           :get,
           "/connected_accounts/get",
           Seam::ConnectedAccount,
           "connected_account",
-          params: { connected_account_id: connected_account_id }
+          params: { connected_account_id: connected_account_id, email: email }.compact
+        )
+      end
+
+      def list(params = {})
+        request_seam_object(
+          :get,
+          "/connected_accounts/list",
+          Seam::ConnectedAccount,
+          "connected_accounts",
+          params: params
         )
       end
     end
