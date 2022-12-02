0118327cf831434fab69cb8557a9ac22c97bc52b
Ravi Chodavarapu <rchodava@gmail.com>
2022-12-02 13:39:16 -0500

    Add device and access code warnings and errors

--- lib/seam/resources/access_code.rb
+++ lib/seam/resources/access_code.rb
@@ -2,8 +2,15 @@
 
 module Seam
   class AccessCode < BaseResource
-    attr_reader :access_code_id, :name, :type, :code
+    attr_reader :access_code_id, :name, :type, :code, :errors, :warnings
 
     date_accessor :starts_at, :ends_at
+
+    def initialize(data, client = nil)
+      super(data, client)
+
+      @errors = data["errors"].is_a?(Array) ? Seam::SeamError.load_from_response(data["errors"]) : []
+      @warnings = data["warnings"].is_a?(Array) ? Seam::Warning.load_from_response(data["warnings"]) : []
+    end
   end
 end
--- lib/seam/resources/device.rb
+++ lib/seam/resources/device.rb
@@ -2,8 +2,15 @@
 
 module Seam
   class Device < BaseResource
-    attr_accessor :device_id, :device_type, :properties
+    attr_accessor :device_id, :device_type, :properties, :errors, :warnings
 
     date_accessor :created_at
+
+    def initialize(data, client = nil)
+      super(data, client)
+
+      @errors = data["errors"].is_a?(Array) ? Seam::SeamError.load_from_response(data["errors"]) : []
+      @warnings = data["warnings"].is_a?(Array) ? Seam::Warning.load_from_response(data["warnings"]) : []
+    end
   end
 end
