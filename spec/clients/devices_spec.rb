da0527e04a914d3c6bfc5684c7a98683d2b86e61
Andrii Balitskyi <10balian10@gmail.com>
2023-09-05 15:23:28 +0200

    Add name param to devices.get method + adjust the test

--- spec/clients/devices_spec.rb
+++ spec/clients/devices_spec.rb
@@ -20,17 +20,34 @@ RSpec.describe Seam::Clients::Devices do
   end
 
   describe "#get" do
-    let(:device_id) { "device_id_1234" }
-    let(:device_hash) { {device_id: device_id} }
+    context "'device_id' param'" do
+      let(:device_id) { "device_id_1234" }
+      let(:device_hash) { {device_id: device_id} }
 
-    before do
-      stub_seam_request(:get, "/devices/get", {device: device_hash}).with(query: {device_id: device_id})
+      before do
+        stub_seam_request(:get, "/devices/get", {device: device_hash}).with(query: {device_id: device_id})
+      end
+
+      let(:result) { client.devices.get(device_id) }
+
+      it "returns a Device" do
+        expect(result).to be_a(Seam::Device)
+      end
     end
 
-    let(:result) { client.devices.get(device_id) }
+    context "'name' param'" do
+      let(:name) { "name_1234" }
+      let(:device_hash) { {name: name} }
 
-    it "returns a Device" do
-      expect(result).to be_a(Seam::Device)
+      before do
+        stub_seam_request(:get, "/devices/get", {device: device_hash}).with(query: {name: name})
+      end
+
+      let(:result) { client.devices.get(name: name) }
+
+      it "returns a Device" do
+        expect(result).to be_a(Seam::Device)
+      end
     end
   end
 
