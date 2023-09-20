e1f11e0f35e9d5531d19623cb904b8be98181a9c
Andrii Balitskyi <10balian10@gmail.com>
2023-09-20 14:06:45 +0200

    Fix UnmanagedAccessCode's attributes

--- lib/seam/resources/unmanaged_access_code.rb
+++ lib/seam/resources/unmanaged_access_code.rb
@@ -2,7 +2,7 @@
 
 module Seam
   class UnmanagedAccessCode < BaseResource
-    attr_accessor :access_code_id, :device_id, :type, :created_at, :name, :code, :is_managed
+    attr_accessor :access_code_id, :device_id, :type, :name, :code, :is_managed, :status
 
     date_accessor :created_at, :starts_at, :ends_at
 
