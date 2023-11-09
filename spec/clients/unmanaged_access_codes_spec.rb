a421ad5510b0a9e0fb58fc340923f5d95af38103
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2023-11-09 17:05:35 +0100

    feat: Add `unmanaged_access_codes.delete` method (#67)
    * Add delete method to unmanaged_access_codes + test
    
    * Fix test
    
    * Run format
    
    ---------
    
    Co-authored-by: Seam Bot <devops@getseam.com>

--- spec/clients/unmanaged_access_codes_spec.rb
+++ spec/clients/unmanaged_access_codes_spec.rb
@@ -25,7 +25,10 @@ RSpec.describe Seam::Clients::UnmanagedAccessCodes do
     let(:unmanaged_access_code_hash) { {access_code_id: "123", device_id: device_id} }
 
     before do
-      stub_seam_request(:post, "/access_codes/unmanaged/list", {access_codes: [unmanaged_access_code_hash]}).with { |req| req.body.source == {device_id: device_id}.to_json }
+      stub_seam_request(:post, "/access_codes/unmanaged/list",
+        {access_codes: [unmanaged_access_code_hash]}).with do |req|
+        req.body.source == {device_id: device_id}.to_json
+      end
     end
 
     let(:unmanaged_access_codes) { client.unmanaged_access_codes.list(device_id) }
@@ -60,6 +63,35 @@ RSpec.describe Seam::Clients::UnmanagedAccessCodes do
     end
 
     let(:result) { client.unmanaged_access_codes.convert_to_managed(access_code_id) }
+
+    it "returns an Action Attempt" do
+      expect(result).to be_a(Seam::ActionAttempt)
+    end
+  end
+
+  describe "#delete" do
+    let(:access_code_id) { "access_code_5678" }
+    let(:action_attempt_hash) { {action_attempt_id: "5678", status: "pending"} }
+
+    before do
+      stub_seam_request(
+        :post, "/access_codes/unmanaged/delete", {action_attempt: action_attempt_hash}
+      ).with do |req|
+        req.body.source == {access_code_id: access_code_id}.to_json
+      end
+
+      stub_seam_request(
+        :post,
+        "/action_attempts/get",
+        {
+          action_attempt: {
+            status: "success"
+          }
+        }
+      ).with { |req| req.body.source == {action_attempt_id: action_attempt_hash[:action_attempt_id]}.to_json }
+    end
+
+    let(:result) { client.unmanaged_access_codes.delete(access_code_id) }
 
     it "returns an Action Attempt" do
       expect(result).to be_a(Seam::ActionAttempt)
